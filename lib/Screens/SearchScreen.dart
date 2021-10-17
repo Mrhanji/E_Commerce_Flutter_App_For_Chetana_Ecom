7f3f56321221a4df6534fe40f3387d54df6750a7
Sagar Sharma <sagarsharma995899@gmail.com>
2021-10-17 21:17:41 +0530

    v1

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shopping/Screens/ProductView.dart';
+import 'package:shopping/Utils/config.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key key}) : super(key: key);
@@ -10,8 +14,10 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   final _txt = TextEditingController();
+  List src;
   @override
   Widget build(BuildContext context) {
+    var size=MediaQuery.of(context).size;
     return MaterialApp(theme: ThemeData(primaryColor: Colors.white,accentColor: Colors.white),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -25,8 +31,33 @@ class _SearchScreenState extends State<SearchScreen> {
           fillColor: Colors.white,icon: Icon(Icons.search),
           
           ),
+              onChanged: (data){
+            final Map<String,dynamic> project={
+              'name':data.toString()
+            };
+            var st=jsonEncode(project);
+             http.post(api+'search.php',headers: {'Accept':'application/json' },body: st).then((value) {
+               if(value.statusCode==200){
+                 setState(() {
+                   src=jsonDecode(value.body);
+                 });
+               }
+             });
+
+              },
         )),
-      )),
+      ),
+      body: ListView(
+        children: src!=null?src.map((e) {
+          return ListTile(
+            title: Text(e['name'],maxLines: 1,overflow: TextOverflow.ellipsis,),
+            leading: Container(height:size.height*0.06,width:size.width*0.2,child: Image.network(e['path'])),
+            trailing: Icon(Icons.arrow_right),
+            onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductView(data: e['id'],))),
+          );
+        }).toList():[]
+      ),
+      ),
     );
   }
 }
